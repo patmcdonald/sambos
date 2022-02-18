006debb334aaa66917499568c8e1d0014e5d6b05
Pat McDonald <patmcdonald@me.com>
2022-02-18 15:29:42 +0000

    why no work in zsh?

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
+from turtle import update
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 # constant variable names written in CAPS
 SCOPE = [
@@ -62,33 +62,11 @@ def update_sales_worksheet(data):
     update sales worksheet, add new row with the list data provided.
     """
 
-    print("Updating sales worksheet...\n")
+    print("updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfull.\n")
+    print("Sales worksheet updated successfully.\n")
 
-def calculate_surplus_data(sales_row):
-    """
-    Compare sales with stock and calculate the surplus for each item type.
-
-    Surplus is defined as the sales figure subtracted from the stock:
-    - Positive surpus indicates waste
-    - Negative surplus indicates extra made when stock was sold out.
-    """
-    print("Calculating surplus data...\n")
-    stock = SHEET.worksheet("stock").get_all_values()
-    stock_row = stock[-1]
-    print(stock_row)
-
-
-def main():
-    """
-    Run all program functions
-    """
-    data = get_sales_data()
-    sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
-
-print("Welcome to sambos data automation")
-main()
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
